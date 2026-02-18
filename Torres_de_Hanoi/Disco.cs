40344345cceca5477f717de6b5418eff09715744
soufiane belfquih El Khalfi <soufianebelfquih828@gmail.com>
2026-02-18 09:39:34 +0100

    Parte Aitor Disco

---
+++ Torres_de_Hanoi/Disco.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torres_de_Hanoi
+{
+    class Disco
+    {
+        public int Valor { get; }
+
+        public Disco(int valor)
+        {
+            if (valor <= 0)
+                throw new ArgumentOutOfRangeException(nameof(valor), "El disco debe tener tamaño > 0.");
+            Valor = valor;
+        }
+
+        public bool PuedeColocarseEncimaDe(int topDestino)
+        {
+            return topDestino == 0 || Valor < topDestino;
+        }
+
+        public override string ToString() => Valor.ToString();
+    }
+}
+
