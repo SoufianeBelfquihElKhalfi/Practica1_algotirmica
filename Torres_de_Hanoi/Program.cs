16eba772f3a5a30eda968c137b7f2dd28910d81b
Soufiane <soufianebelfquih828@gmail.com>
2026-02-19 13:05:30 +0100

    ya funciona

--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -8,39 +8,39 @@ namespace Torres_de_Hanoi
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            
-                // 1. Mensaje de bienvenida
-                Console.WriteLine("El Gran Juego de las Torres de Hanoi");
-                Console.WriteLine("Este juego se juega con 3 torres");
-                Console.WriteLine();
 
-                // 2. Input de Discos
-                Console.Write("Indica el número de discos... ");
+            Pila torreInicial = new Pila();
+            Pila torreFinal = new Pila();
+            Pila torreAuxiliar = new Pila();
+            Hanoi juego = new Hanoi();
+            int movimientos = 0;
 
-                int numeroDiscos;
-                numeroDiscos = int.Parse(Console.ReadLine());
+            // 1. Mensaje de bienvenida
+            Console.WriteLine("El Gran Juego de las Torres de Hanoi");
+            Console.WriteLine("Este juego se juega con 3 torres");
+            Console.WriteLine();
 
-            // hecho manualmente 
-            // Crear un arreglo de discos (del más grande al más pequeño)
-            Disco[] discos = new Disco[numeroDiscos];
-            for (int i = 0; i < numeroDiscos; i++)
+            // 2. Input de Discos
+            Console.Write("Indica el número de discos... ");
+
+            int numeroDiscos;
+            numeroDiscos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPreparando discos...");
+            for (int i = numeroDiscos; i >= 1; i--)
             {
-                // Los discos más grandes tienen valores mayores
-                // Así que el disco de abajo (posición 0) tiene el valor más alto
-                discos[i] = new Disco(numeroDiscos - i);
+                Disco disco = new Disco(i);
+                torreInicial.push(disco);
+                Console.WriteLine($"Disco de tamaño {i} agregado a la torre inicial");
             }
-
-            //Console.WriteLine("\nDiscos creados (del más grande al más pequeño):");
-            //foreach (var disco in discos)
-            //{
-            //    Console.WriteLine($"Disco de tamaño: {disco}");
-            //}
 
             Console.WriteLine("Has seleccionado " + numeroDiscos + " discos");
 
-                
+
 
             // Mensaje para elegir método
             Console.Write("Indica I para Iterativo o R para Recursivo... ");
@@ -51,9 +51,22 @@ namespace Torres_de_Hanoi
             Console.WriteLine("Has seleccionado el método " + metodo);
 
 
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+           
+
+            if (metodo == 'I')
+            {
+                Console.WriteLine("Ejecutando método Iterativo...");
+                movimientos = juego.iterativo(numeroDiscos, torreInicial, torreFinal, torreAuxiliar);
+            }
+            else if (metodo == 'R')
+            {
+                Console.WriteLine("Ejecutando método Recursivo...");
+                movimientos = juego.recursivo(numeroDiscos, torreInicial, torreFinal, torreAuxiliar);
+            }
+            else
+            {
+                Console.WriteLine("Opción no válida. Por favor, selecciona I o R.");
+            }
         }
     }
 }
